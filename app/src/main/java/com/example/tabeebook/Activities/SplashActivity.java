0a6465f1d7d5710582b6980286d08315ca4ed8bb
Mohammed <mgeeks96@gmail.com>
2019-12-30 01:26:31 +0200

    Splash and Logo

---
+++ app/src/main/java/com/example/tabeebook/Activities/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.tabeebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME = 3000; //This is 3 seconds
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                                          startActivity(intent);
+
+                                          finish();
+                                      }
+                                  },
+                SPLASH_TIME);
+
+    }
+}
+
